b0423c9a304b2b9ef076635a1a661d087bd32e7b
Alexey197 <margadonnn3@gmail.com>
2024-05-30 09:07:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+const Counter = () => {
+  const count = 0
+
+  const formatCount = () => {
+    return count === 0 ? 'empty' : count
+  }
+  
+  return (
+    <>
+      <span className="badge bg-primary m-2 p-2">{formatCount()}</span>
+      <button className="btn btn-primary btn-sm m-2">+</button>
+    </>
+  );
+};
+
+export default Counter;
